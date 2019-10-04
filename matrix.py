1a7f307111a093e595feca6a47c1082aee5bc318
ruiruigirl <1147681296@qq.com>
2019-10-04 21:06:49 +0800

    feat: add 矩阵的基础运算和基础性质

--- matrix.py
+++ matrix.py
@@ -5,6 +5,47 @@ class Matrix:
     def __init__(self, list2d):
         self._values = [row[:] for row in list2d]
 
+    def __add__(self, another):
+        """返回两个矩阵的加法结果"""
+        assert self.shape() == another.shape(), \
+            "Error in adding, Shape of matrix must be same."
+        return Matrix([[a + b for a, b in zip(self.row_vector(i), another.row_vector(i))] 
+            for i in range(self.row_num())])
+
+    def __sub__(self, another):
+        """返回两个矩阵的减法结果"""
+        assert self.shape() == another.shape(), \
+            "Error in subbing, Shape of matrix must be same."
+        return Matrix([[a - b for a, b in zip(self.row_vector(i), another.row_vector(i))] 
+            for i in range(self.row_num())])
+
+    def __mul__(self, k):
+        """返回矩阵的数量乘结果 self * k"""
+        return Matrix([[e * k for e in self.row_vector(i)]
+                        for i in range(self.row_num())])
+
+    def __rmul__(self, k):
+        """返回矩阵的数量乘结果  k * self"""
+        return Matrix([[k * self for e in self.row_vector(i)]
+                        for i in range(self.row_num())])    
+
+    @classmethod
+    def zero(cls, r, c):
+        """返回一个 r 行 c 列的零矩阵"""
+        return cls([0] * c for _ in range(r))
+
+    def __truediv__(self, k):
+        """返回数量除法的结果矩阵： self / k"""
+        return (1 / k) * self 
+
+    def __pos__(self):
+        """返回矩阵取正的结果"""
+        return 1 * self
+
+    def __neg__(self):
+        """返回矩阵取反的结果"""
+        return -1 * self           
+
     def __getitem__(self, pos):
         """返回矩阵 pos 位置的元素"""
         r, c = pos
